d97e25cd4e39ccf55d251b7c4f99629c83633cbd
ouvarov <ouvarov@swivl.com>
2021-01-31 22:27:11 +0200

    refactor loading

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
 
-const Loading: React.FunctionComponent = () => (
-    <div className="loader">
-        <div className="loader__wrap">
-            <div className="loader__item" />
-            <div className="loader__item" />
-            <div className="loader__item" />
-            <div className="loader__item" />
-            <div className="loader__item" />
-            <div className="loader__item" />
-            <div className="loader__item" />
-            <div className="loader__item" />
-            <div className="loader__item" />
-            <div className="loader__item" />
+const Loading: React.FunctionComponent = () => {
+    const loaderItem = Array.from({ length: 10 }, (_, idx) => idx);
+
+    return (
+        <div className="loader">
+            <div className="loader__wrap">
+                {loaderItem.map(item => (
+                    <div key={item} className="loader__item" />
+                ))}
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Loading;
